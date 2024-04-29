314e3a7e17eb5b8b0c0590d8a8b0b5f180f95b62
Larsen00 <jonas.larsen103@gmail.com>
2024-04-29 10:31:51 +0200

    pre push

--- PBT/ExpressionPBT.fsx
+++ PBT/ExpressionPBT.fsx
@@ -57,16 +57,8 @@ let _ = Check.Quick treePBT
 // Eval PBT
 
 let evalOperation e1 e2 env f =
-   
-    let bool = eval (f e1 e2) env = (getNumber <| f (eval e1 env |> N) (eval e2 env |> N))
-    if not bool then
-        printfn "\n\nnewloop\n%A" e1
-        printfn "%A" e2
-        printfn "fun: %A" f.ToString
-        printfn "%A = %A " (eval (f e1 e2) env) (getNumber <| f (eval e1 env |> N) (eval e2 env |> N))
-        bool
-        else
-        bool
+   eval (f e1 e2) env = (getNumber <| f (eval e1 env |> N) (eval e2 env |> N))
+
 
 
 let evalPBT ((env ,xlist):SmallEnv) = 
@@ -85,3 +77,21 @@ let evalPBT ((env ,xlist):SmallEnv) =
     |> Prop.classify (result = 3) "OverflowException"
 
 let _ = Check.Quick evalPBT
+
+
+// divisin by zero test
+let div0 ((env ,xlist):SmallEnv) = 
+    let result = 
+        try 
+        let  e = Gen.sample 1 2 (exprGen xlist 10 leafGen) |> List.head
+        let _ = eval e env
+        1
+        with 
+            | :? System.DivideByZeroException as _ -> 2
+            | :? System.OverflowException as _ -> 3
+    (result = 1 || result = 2 || result = 3)
+    |> Prop.classify (result = 1) "Fine"
+    |> Prop.classify (result = 2) "DivideByZeroExceptions"
+    |> Prop.classify (result = 3) "OverflowException"
+    
+let _ = Check.Quick div0

8510da18eabc7633315a1b9c7ad48765b59a9f9d
Larsen00 <jonas.larsen103@gmail.com>
2024-05-09 10:32:09 +0200

    before start commit

--- Program.fs
+++ Program.fs
@@ -1,6 +1,25 @@
+module Program
+
+// open Microsoft.AspNetCore.Hosting
+// open Microsoft.Extensions.Hosting
+// open Giraffe
+
+// let webApp =
+//     choose [
+//         route "/processInput/{input}" >=> Api.processInputHandler
+//         setStatusCode 404 >=> text "Not found"
+//     ]
+
+// let configureApp (app: IApplicationBuilder) =
+//     app.UseRouting().UseEndpoints(fun endpoints -> endpoints.MapGiraffe webApp)
+
+// let configureServices (services: IServiceCollection) =
+//     services.AddRouting()
+
 [<EntryPoint>]
-let main argv =
-    printfn "Hello, world!"
-    0 // return an integer exit code
-
-
+let main args =
+    // WebHost.CreateDefaultBuilder(args)
+    //     .Configure(configureApp)
+    //     .ConfigureServices(configureServices)
+    //     .Build().Run()
+    0
--- obj/Debug/net7.0/main.AssemblyInfo.fs
+++ obj/Debug/net7.0/main.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("main")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fe10e330f2de4adba75d16ec5628337fbb05dc")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("main")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("main")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]

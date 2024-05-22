92fd428734343f6ec139aad4f24a94cd5439e3db
Larsen00 <jonas.larsen103@gmail.com>
2024-05-22 16:47:30 +0200

    save

--- obj/Debug/net7.0/main.AssemblyInfo.fs
+++ obj/Debug/net7.0/main.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("main")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5e22e2f10fe8d91444222974fe0bc9332d1119")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed0ff6ddde7b7e9b04e1506f4bdc3fe557c7054")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("main")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("main")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
